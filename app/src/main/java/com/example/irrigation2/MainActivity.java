e8e570be5ea23254392dae4edc8082bb2233b446
lennedy <lennedy@gmail.com>
2020-04-30 12:06:58 -0300

    primeiro commit

---
+++ app/src/main/java/com/example/irrigation2/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.irrigation2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imgView = (ImageView) findViewById(R.id.imageView8);
+        imgView.setImageResource(R.drawable.circvermelho);
+
+        TextView t = (TextView) findViewById(R.id.textView);
+        t.setText("    Gramado da Frente");
+
+        TextView t2 = (TextView) findViewById(R.id.textView5);
+        t2.setText("    Canteiros da Frente");
+
+        TextView t3 = (TextView) findViewById(R.id.textView4);
+        t3.setText("    Canteiros do Lado");
+
+    }
+
+    public void clickButtom(View view){
+        ImageView imgView = (ImageView) findViewById(R.id.imageView8);
+        imgView.setImageResource(R.drawable.circverde);
+    }
+}
