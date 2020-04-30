3844fdc71c250072519558d3bc10237cd666218e
lennedy <lennedy@gmail.com>
2020-04-30 15:38:17 -0300

    adição do método doInBackground para realizar as atividades em backgroud

--- app/src/main/java/com/example/irrigation2/DownloadJsonAsyncTask.java
+++ app/src/main/java/com/example/irrigation2/DownloadJsonAsyncTask.java
@@ -2,5 +2,12 @@ package com.example.irrigation2;
 
 import android.os.AsyncTask;
 
-public class DownloadJsonAsyncTask extends AsyncTask {
+public class DownloadJsonAsyncTask extends AsyncTask<String,Void, String> {
+    private int cont=0;
+
+    @Override
+    protected String doInBackground(String... params) {
+
+        return (""+cont);
+    }
 }
